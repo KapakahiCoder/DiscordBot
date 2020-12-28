a67ae7b313d6f4eae93bc175586b99b983c38f34
Kapakahi <rubbishman@gmail.com>
2020-12-28 22:56:19 +0900

    add bot to server

--- bot.js
+++ bot.js
@@ -2,5 +2,10 @@ const Discord = require("discord.js");
 require("dotenv").config();
 
 const client = new Discord.Client();
+const discord_token = process.env.DISCORD_TOKEN;
 
-console.log(process.env);
+client.once("ready", () => {
+  console.log("bot is online");
+});
+
+client.login(discord_token);
