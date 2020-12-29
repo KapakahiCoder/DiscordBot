445688e8dab0f5c9d557d026f50a391f457d9b26
Kapakahi <rubbishman@gmail.com>
2020-12-29 17:49:59 +0900

    upload avatar

--- bot.js
+++ bot.js
@@ -8,4 +8,10 @@ client.once("ready", () => {
   console.log("bot is online");
 });
 
+// Set avatar
+client.user
+  .setAvatar("mene.jpg")
+  .then((user) => console.log("New avatar set!"))
+  .catch(console.error);
+
 client.login(discord_token);
