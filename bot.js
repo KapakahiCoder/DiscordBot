75de20b1b2c9a3cef90d20526951f7fed849f7ae
Kapakahi <rubbishman@gmail.com>
2020-12-31 12:43:06 +0900

    take user city and return weather forecast

--- bot.js
+++ bot.js
@@ -20,24 +20,34 @@ client.on("message", (msg) => {
   //Gets the command/prompt from the user
   const args = msg.content.slice(prefix.length).trim().split(" ");
   const command = args.shift().toLowerCase();
+  console.log(args[0]);
+  console.log(command);
 
+  //Greeting
   if (command === "hello" || command === "hi") {
     msg.reply(`Hello ${msg.author.username}`);
   }
+  // ego boast
+  if (command === "ego") {
+    msg.react("😀");
+    msg.reply(`You are amazing ${msg.author.username}`);
+  }
+
+  //Weather forecast
+  const weather_token = process.env.OPEN_WEATHER_API_KEY;
+  const city = args[0];
+  const weatherURL = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${weather_token}`;
+
+  axios
+    .get(weatherURL)
+    .then((response) => {
+      msg.reply(response);
+      console.log(response);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
 });
-
-const weather_token = process.env.OPEN_WEATHER_API_KEY;
-const city = "Honolulu";
-const weatherURL = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${weather_token}`;
-
-axios
-  .get(weatherURL)
-  .then((response) => {
-    console.log(response);
-  })
-  .catch((error) => {
-    console.error(error);
-  });
 
 // Bot login -- keep at end of file
 client.login(discord_token);
